75e7bfb0d5760c33eed69ca4e096c484c3003249
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 15:10:20 +0530

    Add files via upload

---
+++ 01 Oops Theory/01.OOPs.java
@@ -0,0 +1,72 @@
+
+/*
+ * OOPs-> object oriented programming system.
+ * software development methodology which relates to real world.
+ * maintability
+ * flexibily
+ * extensible
+ * reusible code/software
+ * Modularity
+ * data hiding
+ * OOPs concepts:
+ * 1.class
+ * 2.objects
+ * -----main 4 pilars-----
+ * 1.inheritance
+ * 2.encapsulation
+ * 3.polymorphism
+ * 4.abstraction
+ * #Building
+ * blueprint->class->(attribute and behavior)
+ * 1. attributes = states = fields = data = variables
+ * 2. behaviour = methods = functions
+ * material(objects)
+ * space
+ * constructor-->initialize the things.
+ * 
+ * Ques-how many building can build using same blueprint?
+ * Ans-many
+ 
+ * Ques-is all objects attributes and behaviour will same?
+ * Ans-yes
+ 
+ * Ques-how to differentiate two objects?
+ * Ans-every object has it's own unique identity.
+ * 
+ * changing one object attribute and behaviour can't effect other object.
+ 
+ ************CONSTRUCTOR***********
+ * construtor is a specific type of method which is used to initilize the data.
+ * constructor does't have any return type.
+ * constructor is called during the creting the object.
+ * constructor name is same as class name. 
+ * default constructor make by JVM implicity.
+ * 
+ * 
+ * *********KEYWORDS*************
+ * parent class = base class = super class
+ * child class = derived class = sub class
+ * 
+ * *******************************
+ * Inheritance:
+ * 
+ * Inheritance ->inheritance is a phenpmenon in which child inherits(gets)property from our ancestors(parents).
+ * 
+* types of inheritance:
+1. single inheritance
+2. multi-level inheritance
+3. heirarchical inheritance
+(belownot supported by java directly)
+4.multiple inheritance
+5. hybrid inheritance
+
+* Polymorphism:
+Poly-->many    morphs-->forms
+1.static Polymorphism = static binding = early binding = compile time Polymorphs
+2.Dynamic Polymorphism = Dynamic binding = late binding = run time Polymorphs
+
+1.Static Polymorphism is acheives by Method Overloading
+2.Dynamic Polymorphism is acheives by Method Overriding
+*/
+ 
+  
