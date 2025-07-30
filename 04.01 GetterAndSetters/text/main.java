5702cc24ac7b2c51e8a54bd8813f608be91ab2ed
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 17:42:09 +0530

    Add files via upload

---
+++ 04.01 GetterAndSetters/text/main.java
@@ -0,0 +1,19 @@
+package text;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Student s1=new Student();
+		
+		s1.setAge(20);
+		s1.setRollNo(102);
+		s1.setName("mahi vashistha");
+		
+		System.out.println(s1.getAge());
+		System.out.println(s1.getName());
+		System.out.println(s1.getRollNo());
+
+	}
+
+}
