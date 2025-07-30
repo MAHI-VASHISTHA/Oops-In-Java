36a3fb8084340685a5226916df233ce168f04222
MAHI-VASHISTHA <mahivashistha65@gmail.com>
2025-07-30 17:16:28 +0530

    Add files via upload

---
+++ 04 Encapsulation/Package1/Demo.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Demo extends Student{
+
+public static void main(String[] args) {
+		
+		Student st1=new Student();
+		
+		System.out.println(st1.id=102);
+		System.out.println(st1.addr="muzaffarnagar");
+			
+		System.out.println(st1.name="mahi vashistha");
+		
+		st1.read();
+		st1.run();
+	}
+}
---
+++ 04 Encapsulation/Package1/Student.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class Student {
+
+	String name;
+	String addr;
+	int id;
+	
+	 public void  read() {
+		System.out.println("students reading");
+	}
+	
+	void run() {
+		System.out.println("running fast");
+	}
+}
